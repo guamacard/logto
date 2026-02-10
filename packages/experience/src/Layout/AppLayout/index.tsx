80764d063f29c384958e2abcd6214a936c841c9a
Luis <luis.gamez@laguama.com>
2026-02-10 18:56:54 -0500

    v2

--- packages/experience/src/Layout/AppLayout/index.tsx
+++ packages/experience/src/Layout/AppLayout/index.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 import PageContext from '@/Providers/PageContextProvider/PageContext';
 import usePlatform from '@/hooks/use-platform';
-import LogtoSignature from '@/shared/components/LogtoSignature';
+// Import LogtoSignature from '@/shared/components/LogtoSignature';
 import { layoutClassNames } from '@/utils/consts';
 
 import CustomContent from './CustomContent';
@@ -13,7 +13,7 @@ import styles from './index.module.scss';
 const AppLayout = () => {
   const { experienceSettings, theme } = useContext(PageContext);
   const { isMobile } = usePlatform();
-  const hideLogtoBranding = experienceSettings?.hideLogtoBranding === true;
+  // Const hideLogtoBranding = experienceSettings?.hideLogtoBranding === true;
 
   return (
     <div className={styles.viewBox}>
@@ -21,12 +21,12 @@ const AppLayout = () => {
         {!isMobile && <CustomContent className={layoutClassNames.customContent} />}
         <main className={classNames(styles.main, layoutClassNames.mainContent)}>
           <Outlet />
-          {!hideLogtoBranding && (
+          {/* {!hideLogtoBranding && (
             <LogtoSignature
               className={classNames(styles.signature, layoutClassNames.signature)}
               theme={theme}
             />
-          )}
+          )} */}
         </main>
       </div>
     </div>
